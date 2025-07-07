630b80c274a4265874004bcac3e146cda153560b
sudheerkuntumalla <sudheerkuntumalla3004@gmail.com>
2025-07-07 20:42:38 +0530

    py2

---
+++ main2.py
@@ -0,0 +1,26 @@
+name = "shreyas"
+age = 15 
+is_student = True
+weight = 38.5
+print("Name: ", name)
+print("Data type name is: ", type(name))
+print("Age: ", age)
+print("Data type age is: ", type(age))
+print("is_student: ", is_student)
+print("Data type is_student is: ", type(is_student))
+print("weight: ", weight)
+print("Data type weight is: ", type(weight))
+
+age = str(age)
+print(age)
+print("Data type age is: ", type(age))
+
+weight = int(weight)
+print(weight)
+print("Data type weight is: ", type(weight))
+
+is_student = str(is_student)
+print(weight)
+print("Data type is_student is: ", type(is_student))
+
+
